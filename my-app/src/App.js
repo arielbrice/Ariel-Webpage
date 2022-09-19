f02ee0da3440a5d45f7996918cde94aadac7de48
Ariel Brice <arielbric4@gmail.com>
2022-09-19 13:23:58 -0700

    test commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>TESTING WEBPAGFE COMMIT</h1>
+          <h1>TESTING WEBPAGFE COMMIT TEST</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
